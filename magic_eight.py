0e7585b8ecef22d3e61f79fa7cf3c677a306407e
californiasky123 <caroline.n.hodge@gmail.com>
2018-01-29 15:58:39 -0500

    adding in check_question function

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,19 @@
 # start code here...
 def ask_question():
 	user_question=input("What is your question?")
+	return user_question
+
+def check_question(user_question):
+	if user_question[-1]=="?":
+		pass 
+	else: 
+		print("I'm sorry, I can only answer questions")
+
+my_boolean=True
+
+while my_boolean == True: 
+	user_question=ask_question()
+	if user_question == "quit":
+		my_boolean = False 
+	else:
+		check_question(user_question)
